0af9ac35f0b7c6cf14657141a8190560d8f99fc6
David Bos <david.bos@outlook.com>
2024-07-25 22:51:50 +0200

    Counter overflow fixed and counter is now thread safe

--- roswapper/src/main.rs
+++ roswapper/src/main.rs
@@ -1,25 +1,26 @@
-// Libaries
 use rdev::{listen, Event, EventType, Button};
-
-// Variables
-static mut KEY: u8 = 1;
-
-//Functions
+use std::sync::{Arc, Mutex};
 
 // Main - Start
 fn main() {
-    if let Err(error) = listen(callback) {
+    // Wrap the shared state in an Arc<Mutex<_>> for thread-safe access
+    let key = Arc::new(Mutex::new(1));
+
+    // Clone the Arc to pass it to the callback function
+    let key_clone = Arc::clone(&key);
+
+    if let Err(error) = listen(move |event| callback(event, &key_clone)) {
         println!("Error: {:?}", error);
     }
 }
 
 // Detect keys
-fn callback(event: Event) {
+fn callback(event: Event, key: &Arc<Mutex<u8>>) {
     match event.event_type {
         EventType::ButtonPress(button) => {
             match button {
-                Button::Unknown(1) => backward(),
-                Button::Unknown(2) => forward(),
+                Button::Unknown(1) => backward(key),
+                Button::Unknown(2) => forward(key),
                 _ => (),
             }
         },
@@ -28,23 +29,28 @@ fn callback(event: Event) {
 }
 
 // Handle keys
-fn forward() {
-    unsafe {
-        KEY = KEY + 1
+fn forward(key: &Arc<Mutex<u8>>) {
+    let mut key = key.lock().unwrap();
+    if *key == 10 {
+        *key = 1;
+    } else {
+        *key += 1;
     }
-    output();
+    output(*key);
 }
 
-fn backward() {
-    unsafe {
-        KEY = KEY - 1
+fn backward(key: &Arc<Mutex<u8>>) {
+    let mut key = key.lock().unwrap();
+    if *key == 1 {
+        *key = 10;
+    } else {
+        *key -= 1;
     }
-    output();
+    
+    output(*key);
 }
 
 // Handle output
-fn output() {
-    unsafe {
-        println!("{}", KEY);
-    }
+fn output(key: u8) {
+    println!("{}", key);
 }
